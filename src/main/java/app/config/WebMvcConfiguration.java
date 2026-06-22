2d18b6e21a5968817857ce282d04e857ed0ddced
Galina65 <galina.georgieva.uch@abv.bg>
2026-06-22 07:28:05 +0300

    Add web authentication configuration

---
+++ src/main/java/app/config/WebMvcConfiguration.java
@@ -0,0 +1,20 @@
+package app.config;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
+
+@Configuration
+public class WebMvcConfiguration implements WebMvcConfigurer {
+
+    private final AuthenticationInterceptor authenticationInterceptor;
+
+    public WebMvcConfiguration(AuthenticationInterceptor authenticationInterceptor) {
+        this.authenticationInterceptor = authenticationInterceptor;
+    }
+
+    @Override
+    public void addInterceptors(InterceptorRegistry registry) {
+        registry.addInterceptor(authenticationInterceptor);
+    }
+}
